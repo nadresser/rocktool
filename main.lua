4efab2435d8c4a282fd195cad36c25ab68413bb8
nadresser <nadresser@gmail.com>
2025-06-08 16:11:46 -0500

    Update main.lua
    It's now a bag that can be added to, drawn from (randomly) and emptied (to a single white and red rock).

--- main.lua
+++ main.lua
@@ -3,3 +3,67 @@
 new file!
 
 ]]
+io.stdout:setvbuf("no")
+
+bag = {
+	rocks = {
+
+
+	}
+
+}
+
+function bag:addRock(color)
+	bag.rocks[#bag.rocks +1 ] = color
+	-- body
+end
+
+
+
+function bag:showrocks()
+	print(table.concat(bag.rocks, ", "))
+end
+
+function bag:pickARock()
+	print(bag.rocks[math.random(1, #bag.rocks)])
+end
+
+bag:addRock ("red")
+bag:addRock ("white")
+
+
+ --
+math.randomseed(os.time())
+
+function bag:empty()
+	bag.rocks = {}
+
+bag:addRock ("red")
+bag:addRock ("white")
+
+end
+-- graphibcal stuff
+
+
+
+-- controls
+
+function love.keypressed(key)
+	if key == "r" then
+		bag:addRock("red")
+	elseif key == "w" then
+		bag:addRock("white")
+	elseif key == "return" then
+		bag:showrocks()
+		bag:pickARock()
+	elseif key == "e" then
+		bag:empty()
+	end
+
+end
+
+
+bag:showrocks()
+
+bag:pickARock()
+
